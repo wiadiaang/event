e66b93723575502a36651fa3e8f92ecbeb58c2b2
wiadiaang <aang.wiadi@gmail.com>
2023-09-28 21:01:38 +0700

    update https appservices provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->isLocal()) {
+            //if local register your services you require for development
+            // $this->app->register('Barryvdh\Debugbar\ServiceProvider');
+        } else {
+            //else register your services you require for production
+            $this->app['request']->server->set('HTTPS', true);
+        }
+        Schema::defaultStringLength(191);
     }
 
     /**
@@ -19,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // if(env('APP_ENV') !== 'local') {
+        //     $this->app['request']->server->set('HTTPS', env('FORCE_HTTPS_SCHEME'));
+        // }
     }
 }
